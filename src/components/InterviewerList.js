11a20372d6925813564b1856c5cce01d699e29f8
Maryan <maryanay2000@gmail.com>
2023-02-22 14:30:48 -0500

    Finish building the InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import 'components/InterviewerList.scss'
+import InterviewerListItem from "./InterviewerListItem";
+
+// Props
+// interviewers:array - an array of objects as seen above
+// setInterviewer:function - a function that accepts an interviewer id. This function will simply be passed down to the <InterviewerListItem>
+// interviewer:number - a number that represents the id of the currently selected interviewer
+
+export default function InterviewerList(props){
+
+const interviewArray = props.interviewers.map((interviewer) => {
+  return <InterviewerListItem
+  key={interviewer.id}
+  name={interviewer.name}
+  avatar={interviewer.avatar}
+  selected={interviewer.id === props.interviewer}
+  setInterviewer={() => props.setInterviewer(interviewer.id)}/>
+});
+
+return (
+  <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">
+    {interviewArray}
+  </ul>
+</section>
+)
+
+}
